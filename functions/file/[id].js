5ea91ab1397f288ee5f9ee0e4b513a6080a3016f
ericx88 <34431272+ericx88@users.noreply.github.com>
2022-11-30 01:05:29 +0800

    Update [id].js

--- functions/file/[id].js
+++ functions/file/[id].js
@@ -21,7 +21,7 @@ export async function onRequest(context) {  // Contents of context object
         }
     }
      
-     let response = fetch('https://telegra.ph/' + url.pathname + url.search, {
+     const response = fetch('https://telegra.ph/' + url.pathname + url.search, {
          method: request.method,
          headers: request.headers,
          body: request.body,
